7ab05a00d615558725b8157075e7ad719deeb70b
Luiz Franzon <luizfranzon05@hotmail.com>
2023-10-18 18:01:54 -0300

    🚧 Auto calc porcentage based on total fish count

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,14 +2,22 @@
 
 import { Header } from '@/components/Header'
 import { ProgressBar } from '@/components/ProgressBar'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [totalFishes, setTotalFishes] = useState(364)
+  const [porcentage, setPorcentage] = useState(0)
+
+  useEffect(() => {
+    setPorcentage(totalFishes / 21)
+  }, [totalFishes])
+
   return (
     <div>
       <Header />
       <div className="pointer-events-none relative mt-8 flex select-none flex-col items-center justify-center px-8 drop-shadow-2xl ">
-        <ProgressBar progress={0} width={700} />
-        <p className="relative drop-shadow-2xl sm:-top-9 sm:text-2xl">
+        <ProgressBar progress={porcentage} width={700} />
+        <p className="relative -top-8 rounded-md bg-[#0f0d09] p-2 drop-shadow-2xl sm:-top-9 sm:text-2xl">
           {0}/2100
         </p>
       </div>
